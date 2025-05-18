7c64b42c4b40ce6231fdadcfaebdf89ff4b4da6f
LuxWise <jcsanchez.martinez.2020@gmail.com>
2025-05-18 01:04:36 -0500

    fix: notification service

--- src/main/java/com/NotificationService/messaging/NotificationListener.java
+++ src/main/java/com/NotificationService/messaging/NotificationListener.java
@@ -1,6 +1,10 @@
 package com.NotificationService.messaging;
 
+import com.NotificationService.dto.MailRequest;
 import com.NotificationService.service.MailService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
@@ -9,11 +13,15 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class NotificationListener {
     private final MailService mailService;
+    private final ObjectMapper objectMapper;
 
-    @RabbitListener(queues = "notifications.queue")
-    public void handleNotificationEvent(String message) {
-        // NotificationRequest request = objectMapper.readValue(message, NotificationRequest.class);
-        // mailService.sendHtmlEmail(request.getTo(), request.getSubject(), request.getContent());
-        System.out.println("Mensaje recibido: " + message);
+    @RabbitListener(queues = "notifications_service.queue")
+    public void handleNotificationEvent(String message) throws JsonProcessingException, MessagingException {
+        try {
+            MailRequest request = objectMapper.readValue(message, MailRequest.class);
+            mailService.sendHtmlEmail(request.getTo(), request.getSubject(), request.getContent());
+        }
+        catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());        }
     }
 }
